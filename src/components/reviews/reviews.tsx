5b31cbc372cab8e09dac71e1f91e59926616f2d1
Soulzen <alberto.liria@gmail.com>
2024-02-08 17:25:13 +0100

    No carousel / No Reviews

--- src/components/reviews/reviews.tsx
+++ src/components/reviews/reviews.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import styles from "./reviews.module.css"
-import Carousel from "./carousel/carousel"
+// import Carousel from "./carousel/carousel"
 
 import { reviews } from "@/data"
 
@@ -9,7 +9,7 @@ const Reviews = () => {
   return (
     <section className={styles.reviews__section}>
       <h2>Reviews</h2>
-      <Carousel reviews={reviews} />
+      {/* <Carousel reviews={reviews} /> */}
     </section>
   )
 }
