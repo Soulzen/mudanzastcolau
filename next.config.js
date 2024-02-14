ffec783ca61918d962c1fd3569188baaaa76dbed
Soulzen <alberto.liria@gmail.com>
2024-02-14 22:53:34 +0100

    Updated next config for deprecated external domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "lh5.googleusercontent.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "lh5.googleusercontent.com",
+        pathname: "/**"
+      }
+    ]
   }
 }
 
